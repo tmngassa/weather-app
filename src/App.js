b68639cca1e086ea637e0559e2ded5826459db44
tmngassa <tmngassa@hotmail.com>
2021-04-15 15:54:50 -0400

    Added some basic react file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WeekDays from './components/WeekDays'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeekDays/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/apiConfig.js
@@ -0,0 +1,6 @@
+
+export const apiKey =  "524589f12ff60e2a150e470595208862"
+
+// export default {
+//     apiKey
+// }
