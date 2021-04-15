570295999aad2fa33297a7f8d6c83ddf7b4af181
tmngassa <tmngassa@hotmail.com>
2021-04-15 17:36:33 -0400

    Remove redux related code

--- src/components/redux/index.js
+++
@@ -1,1 +0,0 @@
-export * from './WeatherReadings/WeatherReadingsAction'
--- src/components/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import WeatherReadingsReducer from './WeatherReadings/WeatherReadingsReducer'
-
-const rootReducer = combineReducers({
-    weatherReadings: WeatherReadingsReducer
-})
-
-export default rootReducer
